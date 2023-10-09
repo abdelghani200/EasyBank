b1f25716a0ea594251b1a54acd5436216af95abd
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-10-09 23:32:18 +0100

    git commit -m "EAS-72 calcule form simulation"

--- src/main/java/org/example/Services/ServiceSimulation.java
+++ src/main/java/org/example/Services/ServiceSimulation.java
@@ -16,21 +16,13 @@ public class ServiceSimulation {
         double resultat = 0;
 
         try {
-            if (imEmployeService.findByMatricule(simulation.getEmploye().getMatricule()).isPresent()) {
-                if (simulation.getPaiementMensuel() != null && simulation.getCapitalEmprunte() != null) {
-                    double PaiementMensuel = simulation.getPaiementMensuel();
-                    double CapitalEmprunte = simulation.getCapitalEmprunte();
-                    int paiementMensuelNum = simulation.getPaiementMensuelNum();
-
-                    resultat = (PaiementMensuel * (1.2 / 12)) / (1 - Math.pow(1 + 1.2 / 12, -paiementMensuelNum));
-                } else {
-                    System.out.println("Les champs montly_payment et borrowed_capital ne peuvent pas être vides.");
-                }
+            if (simulation.getCapitalEmprunte().toString().isEmpty() || simulation.getPaiementMensuelNum().toString().isEmpty()){
+                System.out.println("Les champs montly_payment et borrowed_capital ne peuvent pas être vides.");
             } else {
-                System.out.println("L'employé avec le matricule spécifié n'existe pas.");
+                resultat = (simulation.getPaiementMensuel() * (1.2 / 12)) / (1 - Math.pow(1 + 1.2 / 12, -simulation.getPaiementMensuelNum()));
             }
-        } catch (Exception e) {
-            System.out.println("Erreur lors de la création de la simulation : " + e.getMessage());
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
         return resultat;
